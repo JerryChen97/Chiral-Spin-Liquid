edfdeb1ba1807750f7ea52ae43dccbeec82bb7c9
JerryChen97 <chenys13@outlook.com>
2021-04-30 10:17:22 -0400

    corrected by Rouchen

--- iDMRG.py
+++ iDMRG.py
@@ -22,23 +22,25 @@ dmrg_params = {
     'max_E_err': 1.e-8,
     'max_S_err': 1.e-7,
     'N_sweeps_check': 2,
-    'max_sweeps': 64,
+    'max_sweeps': 100,
     'verbose': 1,
+    'chi_list': {0: 100, 10: 1000},
 }
 
 model_params = {
-    'Lx': 1, 'Ly': 4, 'bc':"periodic",# Ly is set below
-    'bc_MPS': "infinite",
+    'Lx': 1, 'Ly': 8, 'bc':'periodic',# Ly is set below
+    'bc_MPS': 'infinite',
     'verbose': 1,
     'cons_N': 'N',
     'cons_Sz': 'Sz',
-    't':1,
-    'td':0.5j
+    't': 1,
+    'td': 0.5j,
 }
 
 
+
 class twosites(Lattice):
-    def __init__(self, Lx, Ly, sites, **kwargs):
+    def __init__(self, Lx, Ly, site, **kwargs):
      
        
         NN = [(0, 1, np.array([0, 0])), (1, 0, np.array([1, 0])), (0, 0, np.array([0, 1]))]
@@ -48,7 +50,7 @@ class twosites(Lattice):
         kwargs['pairs'].setdefault('nearest_neighbors', NN)
         kwargs['pairs'].setdefault('next_nearest_neighbors', NNa)
         kwargs['pairs'].setdefault('next_next_nearest_neighbors', nNN)
-        Lattice.__init__(self, [Lx, Ly], sites, **kwargs)
+        Lattice.__init__(self, [Lx, Ly], site, **kwargs)
 
 
 
@@ -69,28 +71,30 @@ class FermionicChiral(CouplingMPOModel):
         Lx = model_params.get('Lx',1)
         Ly = model_params.get('Ly',8)
         fs = self.init_sites(model_params)
-        bc = model_params.get('bc',"periodic")
-        bc_MPS = model_params.get('bc_MPS',"infinite")
+        bc = model_params.get('bc','periodic')
+        bc_MPS = model_params.get('bc_MPS','infinite')
+       
+
         lat = twosites(Lx, Ly, [fs,fs],bc=bc, bc_MPS=bc_MPS)
         return lat
 
     def init_terms(self, model_params):
         
-        t = np.asarray(model_params.get('t', 1.))
-        td = np.asarray(model_params.get('td', 0.5j))
+        t = model_params.get('t', 1.)
+        td = model_params.get('td', 0.5j)
        
 
         for u1, u2, dx in self.lat.pairs['nearest_neighbors']:
-            self.add_coupling(t , u1, 'Cdu', u2, 'Cu', dx, 'JW', True)
-            self.add_coupling(t , u1, 'Cdd', u2, 'Cd', dx, 'JW', True)
+            self.add_coupling(t , u1, 'Cdu', u2, 'Cu', dx, plus_hc=True)
+            self.add_coupling(t , u1, 'Cdd', u2, 'Cd', dx, plus_hc=True)
             
         for u1, u2, dx in self.lat.pairs['next_nearest_neighbors']:
-            self.add_coupling(-t, u1, 'Cdu', u2, 'Cu', dx, 'JW', True)
-            self.add_coupling(-t, u1, 'Cdd', u2, 'Cd', dx, 'JW', True)
+            self.add_coupling(-t, u1, 'Cdu', u2, 'Cu', dx, plus_hc=True)
+            self.add_coupling(-t, u1, 'Cdd', u2, 'Cd', dx, plus_hc=True)
             
         for u1, u2, dx in self.lat.pairs['next_next_nearest_neighbors']:
-            self.add_coupling(td, u1, 'Cdu', u2, 'Cu', dx, 'JW', True)
-            self.add_coupling(td, u1, 'Cdd', u2, 'Cd', dx, 'JW', True)
+            self.add_coupling(td, u1, 'Cdu', u2, 'Cu', dx, plus_hc=True)
+            self.add_coupling(td, u1, 'Cdd', u2, 'Cd', dx, plus_hc=True)
 
       
             
@@ -105,6 +109,8 @@ psi = MPS.from_lat_product_state(CSLnoGPO.lat, [[["up","down"]]],bc=CSLnoGPO.lat
             
             
 eng = TwoSiteDMRGEngine(psi, CSLnoGPO, dmrg_params)
+
+
 
 E, psi = eng.run()
 
